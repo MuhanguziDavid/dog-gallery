c8dc77f8bd2340c461be5b3024b60bd1a15b32dc
David Muhanguzi <muhunksdavie95@gmail.com>
2023-02-15 14:07:34 +0300

    setup project and write guiding tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders a dogs gallery', () => {
+  throw new Error();
+})
+
+test('renders a dog breed gallery', () => {
+  throw new Error();
+})
+
+test('clicking dog breed in the list opens a modal', () => {
+  throw new Error();
+})
+
+test('modal once opened contains a picture of the dog and its name', () => {
+  throw new Error();
+})
+
+test('clicking a photo in the gallery opens a gallery of pictures of that breed on a new page', () => {
+  throw new Error();
+})
